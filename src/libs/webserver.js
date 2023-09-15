a11a1ea6f98d61040e8a76c0e87f18d6bd7af303
Liberland Software Foundation <info@libersoft.org>
2023-09-16 00:05:37 +0200

    First commit

--- src/libs/webserver.js
+++ src/libs/webserver.js
@@ -11,38 +11,41 @@ class WebServer {
  async run() {
   const data = new Data();
   let streams = await data.getStreams();
-  for (let i = 0; i < streams.length; i++) {
-   console.log(streams[i]);
-  }
-  /*
-  app.use('/stream', (req, res) => {
-   res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
-   let ffmpegCommand = ffmpeg();
-   switch (streams) {
-    case 'webcam':
-     ffmpegCommand = ffmpegCommand.input(settingsFile.sourcePath).inputFormat('v4l2');
-     break;
-    case 'ipcam':
-     ffmpegCommand = ffmpegCommand.input(settingsFile.ipcamURL).inputFormat('rtsp');
-     break;
-    case 'files':
-     for (const file of settingsFile.videoFiles) { ffmpegCommand = ffmpegCommand.input(file); }
-     ffmpegCommand = ffmpegCommand.loop();
-     break;
-    default:
-     throw new Error('Unknown source type');
-   }
-   ffmpegCommand.outputOptions(['-preset ultrafast', '-g 48', '-sc_threshold 0', '-hls_time 10', '-hls_list_size 0', '-f hls'])
-    .toFormat('hls')
-    .on('end', () => { Common.addLog('Stream finished.'); })
-    .pipe(res, { end: true });
+  streams.forEach(stream => {
+   this.addStream(stream, data);
   });
   if (fs.existsSync(socketPath)) { fs.unlinkSync(socketPath); }
   app.listen(socketPath, () => {
    Common.addLog('Server running on Unix socket: ' + socketPath);
   });
   fs.chmodSync(socketPath, '777');
-  */
+ }
+
+ addStream(stream, data) {
+  app.use('/stream/:' + stream.id.toString(), async (req, res) => {
+   console.log(stream);
+   res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+   let ffmpegCommand = ffmpeg();
+   switch (stream.id_source) {
+    case 1:
+     ffmpegCommand = ffmpegCommand.input(stream.path).inputFormat('v4l2');
+     break;
+    case 2:
+     ffmpegCommand = ffmpegCommand.input(stream.path).inputFormat('rtsp');
+     break;
+    case 3:
+     let files = await data.getFiles(stream.id);
+     console.log('AA');
+     console.log(files);
+     console.log('BB');
+     for (const file of files) { ffmpegCommand = ffmpegCommand.input(file); }
+     ffmpegCommand = ffmpegCommand.loop();
+     break;
+    default:
+     Common.addLog('Unknown stream type', 2);
+   }
+   ffmpegCommand.outputOptions(['-preset ultrafast', '-g 48', '-sc_threshold 0', '-hls_time 10', '-hls_list_size 0', '-f hls']).toFormat('hls').on('end', () => { Common.addLog('Stream finished.'); }).pipe(res, { end: true });
+  });
  }
 }
 
